077f51ea338bd6ee4827f6a1a342929349ace586
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-08-28 17:03:47 +0900

    first setup

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+import '../css/style.scss'
+import * as PIXI from 'pixi.js'
+
+const type = PIXI.utils.isWebGLSupported() ? 'WebGL' : 'canvas'
+PIXI.utils.sayHello(type)
+
+const app = new PIXI.Application({
+    width: 1600,
+    height: 900,
+    backgroundColor: 0xffc08b,
+    resolution: devicePixelRatio || 1,
+});
+
+document.body.appendChild(app.view);
+
+
